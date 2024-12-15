e97826cb72ee76f9733c5dc6de98ae5c38633f61
BAD MUNDA <139567638+badmunda011@users.noreply.github.com>
2024-12-15 16:36:23 +0530

    Update __init__.py

--- Quiz/__init__.py
+++ Quiz/__init__.py
@@ -8,6 +8,6 @@ app = Client(
          api_id=API_ID,
          api_hash=API_HASH,
          bot_token=BOT_TOKEN,
-         plugins=dict(root="Quiz.plugins")  # Ensure the plugins folder is structured correctly
+         plugins=dict(root="Quiz.plugins.tools")  # Ensure the plugins folder is structured correctly
 )
 )
