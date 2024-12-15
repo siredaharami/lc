d786e8b87cdaad63062f6c845fba35e18ab534b3
nortoxs <vivekkumar89751@gmail.com>
2024-12-15 13:05:57 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+import re
+import sys
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+# Get it from my.telegram.org
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH")
+
+## Get it from @Botfather in Telegram.
+BOT_TOKEN = getenv("BOT_TOKEN")
+
+# Your User ID.
+OWNER_ID = list(
+    map(int, getenv("OWNER_ID", "").split())
+)  # Input type must be interger
+
+### DONT TOUCH or EDIT codes after this line
+BANNED_USERS = filters.user()
