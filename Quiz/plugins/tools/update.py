7d254ba02b3422860230e67ba813c0db4060212e
nortoxs <vivekkumar89751@gmail.com>
2024-12-15 15:13:22 +0530

    Update update.py

--- Quiz/plugins/tools/update.py
+++ Quiz/plugins/tools/update.py
@@ -1,41 +1,51 @@
+from config import *
 import subprocess
 import os
 import sys
 from pyrogram import Client, filters
+from git import Repo
+from git.exc import GitCommandError, InvalidGitRepositoryError
 
-# Your GitHub repository URL
-GITHUB_REPO = 'https://github.com/nortoxs/Quizbot'
-
-
-# Function to clone the repository if it doesn't exist
-def ensure_repo_exists():
-    repo_name = GITHUB_REPO.split("/")[-1].replace(".git", "")
-    if not os.path.exists(repo_name):
-        print("Cloning the repository...")
-        subprocess.run(["git", "clone", GITHUB_REPO], check=True)
-    os.chdir(repo_name)  # Change to the repository directory
-
-# Git pull and restart function
-def git_pull_and_restart():
-    # Ensure the repository exists
-    ensure_repo_exists()
-    
-    # Pull the latest changes from the repository
-    print("Pulling the latest changes from the repository...")
-    subprocess.run(["git", "pull"], check=True)
-    
-    # Restart the bot
-    print("Restarting the bot...")
-    os.execv(sys.executable, ['python'] + sys.argv)
-
-# Command to trigger git pull and restart
-@app.on_message(filters.command("pull", prefixes="/"))
-async def git_pull_command(client, message):
-    # Only allow authorized user to use this
-    allowed_user_id = 7648939888  # Replace with your user ID
-
-    if message.from_user.id == allowed_user_id:
-        git_pull_and_restart()
-        await message.reply("Bot is updating and restarting.")
+@app.on_message(filters.command("update") & filters.user(OWNER_ID))
+async def update_repo_latest(client, message):
+    response = await message.reply_text("Checking for available updates...")
+    try:
+        repo = Repo()
+    except GitCommandError:
+        return await response.edit("Git Command Error")
+    except InvalidGitRepositoryError:
+        return await response.edit("Invalid Git Repsitory")
+    to_exc = f"git fetch origin aditya &> /dev/null"
+    os.system(to_exc)
+    await asyncio.sleep(7)
+    verification = ""
+    REPO_ = repo.remotes.origin.url.split(".git")[0]  # main git repository
+    for checks in repo.iter_commits(f"HEAD..origin/main"):
+        verification = str(checks.count())
+    if verification == "":
+        return await response.edit("Bot is up-to-date!")
+    updates = ""
+    ordinal = lambda format: "%d%s" % (
+        format,
+        "tsnrhtdd"[(format // 10 % 10 != 1) * (format % 10 < 4) * format % 10 :: 4],
+    )
+    for info in repo.iter_commits(f"HEAD..origin/main"):
+        updates += f"<b>➣ #{info.count()}: [{info.summary}]({REPO_}/commit/{info}) by -> {info.author}</b>\n\t\t\t\t<b>➥ Commited on:</b> {ordinal(int(datetime.fromtimestamp(info.committed_date).strftime('%d')))} {datetime.fromtimestamp(info.committed_date).strftime('%b')}, {datetime.fromtimestamp(info.committed_date).strftime('%Y')}\n\n"
+    _update_response_ = "<b>A new update is available for the Bot!</b>\n\n➣ Pushing Updates Now</code>\n\n**<u>Updates:</u>**\n\n"
+    _final_updates_ = _update_response_ + updates
+    if len(_final_updates_) > 4096:
+        link = await paste_queue(updates)
+        url = link + "/index.txt"
+        nrs = await response.edit(
+            f"<b>A new update is available for the Bot!</b>\n\n➣ Pushing Updates Now</code>\n\n**<u>Updates:</u>**\n\n[Click Here to checkout Updates]({url})"
+        )
     else:
-        await message.reply("You are not authorized to use this command.")
+        nrs = await response.edit(_final_updates_, disable_web_page_preview=True)
+    os.system("git stash &> /dev/null && git pull")
+    await response.edit(
+        f"{nrs.text}\n\nBot was updated successfully! Now, wait for 1 - 2 mins until the bot reboots!"
+    )
+    os.system("pip3 install -r requirements.txt --force-reinstall")
+    os.system(f"kill -9 {os.getpid()} && python3 -m Quiz")
+    sys.exit()
+    return
