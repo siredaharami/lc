72c324f4fb63b4a9bdfb5745fba52c9c0e17d103
nortoxs <vivekkumar89751@gmail.com>
2024-12-15 15:52:55 +0530

    Update __main__.py

--- Quiz/__main__.py
+++ Quiz/__main__.py
@@ -7,15 +7,13 @@ from Quiz.plugins import ALL_MODULES
 loop = asyncio.get_event_loop()
 
 async def main():
-      await app.start()
-      for all_module in ALL_MODULES:
+    await app.start()
+    for all_module in ALL_MODULES:
         importlib.import_module("Quiz.plugins" + all_module)
-      LOGGER("Quiz.plugins").info(
-        "Successfully Imported Modules"
-      )
-      LOGGER("Quiz").info("Quiz Bot Started Successfully")
-      await idle()
-  
+    LOGGER.info("Successfully Imported Modules")
+    LOGGER.info("Quiz Bot Started Successfully")
+    await idle()
+
 # Start the bot
 if __name__ == "__main__":
     print("Quiz bot started successfully")
