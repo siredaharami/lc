c3e5568d6a55b5a49b42128eecd7a645f170dc96
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-29 21:38:31 +0530

    Update start.py

--- Quiz/plugins/start.py
+++ Quiz/plugins/start.py
@@ -1,9 +1,13 @@
 from pyrogram import Client, filters
-from pyrogram.handlers import MessageHandler, CallbackQueryHandler
-from Quiz.plugins.handlers import start_command, handle_photo, handle_text, handle_color_selection
+from Quiz.plugins.handlers import handle_photo, handle_text, handle_callback_query
 from Quiz import app
 
-app.add_handler(MessageHandler(start_command, filters.command("start")))
-app.add_handler(MessageHandler(handle_photo, filters.photo))
-app.add_handler(MessageHandler(handle_text, filters.text & ~filters.command("start")))  # Fix for text filter
-app.add_handler(CallbackQueryHandler(handle_color_selection))
+users_data = {}
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    await message.reply_text("Welcome to the Logo Maker Bot!\nPlease send me an image.")
+
+app.add_handler(handle_photo)
+app.add_handler(handle_text)
+app.add_handler(handle_callback_query)
