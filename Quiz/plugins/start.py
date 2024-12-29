671bee890d5a993435833ce0ea0f459bc5e499a6
siredaharami <123292087+siredaharami@users.noreply.github.com>
2024-12-29 21:10:52 +0530

    Update start.py

--- Quiz/plugins/start.py
+++ Quiz/plugins/start.py
@@ -3,8 +3,7 @@ from pyrogram.handlers import MessageHandler, CallbackQueryHandler
 from Quiz.plugins.handlers import start_command, handle_photo, handle_text, handle_color_selection
 from Quiz import app
 
-# Register handlers
 app.add_handler(MessageHandler(start_command, filters.command("start")))
 app.add_handler(MessageHandler(handle_photo, filters.photo))
-app.add_handler(MessageHandler(handle_text, filters.text & ~filters.command))
+app.add_handler(MessageHandler(handle_text, filters.text & ~filters.command("start")))  # Fix for text filter
 app.add_handler(CallbackQueryHandler(handle_color_selection))
